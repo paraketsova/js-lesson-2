1fd59cff12c3912527167d8dc5b9c44771f850db
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-02 10:53:32 +0100

    Update bootstrap style

--- webb20-js-lesson-2-react/src/App.js
+++ webb20-js-lesson-2-react/src/App.js
@@ -6,25 +6,25 @@ function App() {
 
   const tweetList = [
     {
-      "imageURL": "https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+      "imageURL": "https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80",
       "content": "My Awesome tweet",
       "author": "John",
       "date": "2020-12-03"
     },
     {
-      "imageURL": "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+      "imageURL": "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80",
       "content": "It smells like christmas",
       "author": "Jane",
       "date": "2020-12-02"
     },
     {
-      "imageURL": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+      "imageURL": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80",
       "content": "T-22 days until christmas",
       "author": "Eliza",
       "date": "2020-12-02"
     },
     {
-      "imageURL": "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+      "imageURL": "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80",
       "content": "Dressing up like Santa Claus tomorrow",
       "author": "Eliza",
       "date": "2020-12-01"
--- webb20-js-lesson-2-react/src/components/Tweet.jsx
+++ webb20-js-lesson-2-react/src/components/Tweet.jsx
@@ -7,14 +7,14 @@ import Thumbnail from './Thumbnail'
 export default function Tweet({author, date, content, imageURL}) {
     return (
         <div className='row p-3 border mb-2'>
-            <div className='col-sm-2'>
+            <div className='col-sm-2 col-xs-12 col-md-5'>
                 <Thumbnail imageURL={imageURL} />
             </div>
-            <div className='col-sm-6'>
+            <div className='col-sm-6 col-xs-12 col-md-4'>
                 <Author author={author} />
                 <Content content={content} />
             </div>
-            <div className='col-sm-4'>
+            <div className='col-sm-4 col-xs-12 col-md-3'>
                 <Date date={date} />
             </div>
         </div>
